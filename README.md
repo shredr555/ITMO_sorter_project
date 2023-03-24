dc182b65186ffb4ffb43713b851df696c636ed71
shredr555 <combat-marat@mail.ru>
2023-03-25 00:13:29 +0300

    add readme and small details

---
+++ README.md
@@ -0,0 +1,30 @@
+<h1 align="center">title name</h1>
+<h3 align="center">A passionate frontend developer from India</h3>
+
+- 🔭 I’m currently working on [project name 1](project link 1)
+
+- 🌱 I’m currently learning **framework**
+
+- 👯 I’m looking to collaborate on [project name 2](project link 2)
+
+- 🤝 I’m looking for help with [project name 3](project link 3)
+
+- 👨‍💻 All of my projects are available at [portfolio link](portfolio link)
+
+- 📝 I regularly write articles on [blog link](blog link)
+
+- 💬 Ask me about **react, vue...**
+
+- 📫 How to reach me **example@gmail.com**
+
+- 📄 Know about my experiences [resume link](resume link)
+
+- ⚡ Fun fact **I think i'm funny**
+
+<h3 align="left">Connect with me:</h3>
+<p align="left">
+</p>
+
+<h3 align="left">Languages and Tools:</h3>
+<p align="left"> <a href="https://git-scm.com/" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg" alt="git" width="40" height="40"/> </a> <a href="https://www.linux.org/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/linux/linux-original.svg" alt="linux" width="40" height="40"/> </a> <a href="https://opencv.org/" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/opencv/opencv-icon.svg" alt="opencv" width="40" height="40"/> </a> <a href="https://www.python.org" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg" alt="python" width="40" height="40"/> </a> </p>
+
--- sorter_control/src/simple_move.py
+++ sorter_control/src/simple_move.py
@@ -11,42 +11,37 @@ from geometry_msgs.msg import Twist
 class Follower:
   def __init__(self):
     self.bridge = cv_bridge.CvBridge()
-    #cv2.namedWindow("window", 1)
-    self.image_sub = rospy.Subscriber('/sorter/camera1/image_raw', 
-                                      Image, self.image_callback)
-    self.cmd_vel_pub = rospy.Publisher('/cmd_vel',
-                                       Twist, queue_size=1)
+    self.image_sub = rospy.Subscriber('/sorter/camera1/image_raw', Image, self.image_callback)
+    self.cmd_vel_pub = rospy.Publisher('/sorter/cmd_vel', Twist, queue_size=1)
     self.twist = Twist()
         
 
   def image_callback(self, msg):
     image = self.bridge.imgmsg_to_cv2(msg,desired_encoding='bgr8')
-    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV) 
-    # change below lines to map the color you wanted robot to follow
-    lower_yellow = numpy.array([ 44,  100,  100])
-    upper_yellow = numpy.array([80, 100, 100])
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    lower_yellow = numpy.asarray([0, 0, 255])
+    upper_yellow = numpy.asarray([30, 255, 255])
     mask = cv2.inRange(hsv, lower_yellow, upper_yellow)
     
     h, w, d = image.shape
     search_top = 3*h/4
-    search_bot = 3*h/4 + 20
+    search_bot = 3*h/4 + 100
     mask[0:search_top, 0:w] = 0
     mask[search_bot:h, 0:w] = 0
     M = cv2.moments(mask)
     if M['m00'] > 0:
       cx = int(M['m10']/M['m00'])
       cy = int(M['m01']/M['m00'])
-      #cv2.circle(image, (cx, cy), 20, (0,0,255), -1)
-      # CONTROL starts
-      err = cx - w/2
-      self.twist.linear.x = 0.2
-      self.twist.angular.z = -float(err) / 100
+      
+      err = cx - w/2.5
+      self.twist.linear.x = -1
+      self.twist.angular.z = float(err) / 100
       self.cmd_vel_pub.publish(self.twist)
-      # CONTROL ends
     else:
       self.twist.linear.x = 1
       self.cmd_vel_pub.publish(self.twist)
-      
+    
+    cv2.circle(image, (cx, cy), 20, (0,0,255), -1)
     cv2.imshow("mask",mask)
     cv2.imshow("output", image)
     cv2.waitKey(3)
